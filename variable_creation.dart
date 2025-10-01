85e947f54a946ef32bad3baf1426eaf0ca7a7b6d
ihsanerturkk <ihsanerturk2026@gmail.com>
2025-10-02 00:10:11 +0300

    feat: project initialize

---
+++ variable_creation.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  // (Ihsan = String, 21 = int, 1.75 = double)
+var ogrenciAdi = "Ihsan";
+var ogrenciYasi = 21;
+var ogrenciBoyu = 1.75;
+
+print(ogrenciBoyu);
+print(ogrenciYasi);
+print(ogrenciAdi);
+
+}
