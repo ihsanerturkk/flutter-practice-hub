eb3c55e4112ab8fd8e7405fe67fac7893bb2cc98
ihsanerturkk <ihsanerturk2026@gmail.com>
2025-10-03 12:23:58 +0300

    feat: project initialize

---
+++ lib/section_2_variables_data_types/using_the_print_method.dart
@@ -0,0 +1,23 @@
+void main(){
+
+  int a = 10;
+  int b = 20;
+
+  print("a + b equals: ${a+b}");
+
+  // Sample application procedures
+  
+  String name = "Ahmet";
+  int phoneNumber = 5334567689;
+  var mail = "userMail@gmail.com";
+  
+  print("Persons name: $name");
+  print("Persons phone number: $phoneNumber");
+  print("Persons mail: $mail");
+
+
+
+
+
+
+}
